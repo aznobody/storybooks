2a2046077272f5900a145c18022b8777c3657300
naveen verma <naveenverma088@gmail.com>
2019-01-07 02:04:14 +0530

    done

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,12 +1,6 @@
-// const keys = {
-//     mongoURI: process.env.MONGO_URI,
-//     client_id: process.env.GOOGLE_CLIENT_ID,
-//     client_secret: process.env.GOOGLE_CLIENT_SECRET
-// }
 const keys = {
-    mongoURI: "mongodb://naveen19:naveen19@ds243344.mlab.com:43344/storybooks-dev",
-    client_id: "462437024563-h0blv5nqb36lmeqdr5711465t33glqs1.apps.googleusercontent.com",
-    client_secret: "RrvkSLBM4NwL4geUQ1nguEdg"
+    mongoURI: process.env.MONGO_URI,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET
 }
-
 module.exports = keys;
